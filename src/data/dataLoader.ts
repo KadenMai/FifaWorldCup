85aaee2e8c3beec23e14033b0f3086f964fa54b7
KadenMai <kaden.mai@hotmail.com>
2026-06-25 21:00:08 -0700

    chore(data): update match-024 (0-0 Finished)

--- src/data/dataLoader.ts
+++ src/data/dataLoader.ts
@@ -9,7 +9,7 @@ import type {
 } from '../types';
 
 /** Bump when JSON data changes to bust browser/CDN cache */
-const DATA_VERSION = '202606260104';
+const DATA_VERSION = '202606260400';
 
 let runtimeDataVersion = DATA_VERSION;
 
